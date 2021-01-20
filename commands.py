de86a4ffb8e423f0c2b4eb98891eb7a6fcf730f5
Zekimar <zzffcrz@gmail.com>
2021-01-19 19:47:13 -0500

    changed server variable to context for clarity

--- commands.py
+++ commands.py
@@ -5,71 +5,71 @@ from discord.ext.commands import *
 
 info = "```This Bot was created by Kevin Good using Python with the discord.py API```"
 
-#a cog is a class of bot commands according to the API
+#a cog is a class of bot commands
 class basic_commands(Cog):
     def __init__(self, bot):
         self.bot = bot
 
     #example command
-    #server = context of command aka the server it came from
+    #context = context object given back by API with various data
     #arg = everything said after the command
     # * means give the rest of the text as a single argument
     @command()
-    async def mirror(self, server, *, arg):
-        await server.send(arg)
+    async def mirror(self, context, *, arg):
+        await context.send(arg)
 
     #@command denotes that the following function is a command for the bot
-    #server = context of message
-    #server.send("PONG!") will send "PONG!" to the server the command originated from
+    #context = context of message
+    #context.send("PONG!") will send "PONG!" to the context the command originated from
     @command()
-    async def ping(self, server):
-        await server.send("PONG!")
+    async def ping(self, context):
+        await context.send("PONG!")
 
     @command()
-    async def test(self, server):
-        await server.send("sucess!")
+    async def test(self, context):
+        await context.send("sucess!")
 
     @command()
-    async def info(self, server):
-        await server.send(info);
+    async def info(self, context):
+        await context.send(info);
 
     #max: int is a user defined argument for this command
     #defaults to 6
     @command()
-    async def roll(self, server, max = 6):
+    async def roll(self, context, max = 6):
         ans = randint(1, max)
-        await server.send(ans)
+        await context.send(ans)
     #commands come with local error handlers
     @roll.error
-    async def roll_error(self, server, error):
+    async def roll_error(self, context, error):
         if isinstance(error, BadArgument):
-            await server.send("ERROR: bad argument")
+            await context.send("ERROR: bad argument")
 
     @command()
-    async def flip(self, server):
+    async def flip(self, context):
         result = randint(0,1)
         if result == 0:
-            await server.send("heads!")
+            await context.send("heads!")
         else:
-            await server.send("tails!")
+            await context.send("tails!")
 
     @command()
-    async def nick(self, server, newnick):
-        await server.guild.me.edit(nick = newnick)
-        await server.send("nickname changed to " + member.display_name)
+    async def nick(self, context, newnick):
+        await context.guild.me.edit(nick = newnick)
+        await context.send("nickname changed to " + member.display_name)
     @nick.error
-    async def nick_error(self, server, error):
+    async def nick_error(self, context, error):
         if isinstance(error, MissingRequiredArgument):
-            await server.send("ERROR: missing argument.")
+            await context.send("ERROR: missing argument.")
 
-class server_info(Cog):
+class context_info(Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @command()
-    async def emojis(self, server):
-        emojis = server.guild.emojis
-        message = "server emojis:\n"
+    async def emojis(self, context):
+        emojis = context.guild.emojis
+        message = "context emojis:\n"
         for emoji in emojis:
             message += "<:" + emoji.name + ":" + str(emoji.id) + ">"
-        await server.send(message)
+        await context.send(message)
