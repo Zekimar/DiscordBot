31cf73454593ec0593fc8e23beca9c4494809b90
Zekimar <zzffcrz@gmail.com>
2021-01-19 19:38:30 -0500

    changed ctx variable name to server for clarity

--- commands.py
+++ commands.py
@@ -11,7 +11,7 @@ class basic_commands(Cog):
         self.bot = bot
 
     #example command
-    #ctx = context of command aka the server it came from
+    #server = context of command aka the server it came from
     #arg = everything said after the command
     # * means give the rest of the text as a single argument
     @command()
@@ -19,57 +19,57 @@ class basic_commands(Cog):
         await server.send(arg)
 
     #@command denotes that the following function is a command for the bot
-    #ctx = context of message
-    #ctx.send("PONG!") will send "PONG!" to the server the command originated from
+    #server = context of message
+    #server.send("PONG!") will send "PONG!" to the server the command originated from
     @command()
-    async def ping(self, ctx):
-        await ctx.send("PONG!")
+    async def ping(self, server):
+        await server.send("PONG!")
 
     @command()
-    async def test(self, ctx):
-        await ctx.send("sucess!")
+    async def test(self, server):
+        await server.send("sucess!")
 
     @command()
-    async def info(self, ctx):
-        await ctx.send(info);
+    async def info(self, server):
+        await server.send(info);
 
     #max: int is a user defined argument for this command
     #defaults to 6
     @command()
-    async def roll(self, ctx, max = 6):
+    async def roll(self, server, max = 6):
         ans = randint(1, max)
-        await ctx.send(ans)
+        await server.send(ans)
     #commands come with local error handlers
     @roll.error
-    async def roll_error(self, ctx, error):
+    async def roll_error(self, server, error):
         if isinstance(error, BadArgument):
-            await ctx.send("ERROR: bad argument")
+            await server.send("ERROR: bad argument")
 
     @command()
-    async def flip(self, ctx):
+    async def flip(self, server):
         result = randint(0,1)
         if result == 0:
-            await ctx.send("heads!")
+            await server.send("heads!")
         else:
-            await ctx.send("tails!")
+            await server.send("tails!")
 
     @command()
-    async def nick(self, ctx, newnick):
-        await ctx.guild.me.edit(nick = newnick)
-        await ctx.send("nickname changed to " + member.display_name)
+    async def nick(self, server, newnick):
+        await server.guild.me.edit(nick = newnick)
+        await server.send("nickname changed to " + member.display_name)
     @nick.error
-    async def nick_error(self, ctx, error):
+    async def nick_error(self, server, error):
         if isinstance(error, MissingRequiredArgument):
-            await ctx.send("ERROR: missing argument.")
+            await server.send("ERROR: missing argument.")
 
 class server_info(Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @command()
-    async def emojis(self, ctx):
-        emojis = ctx.guild.emojis
+    async def emojis(self, server):
+        emojis = server.guild.emojis
         message = "server emojis:\n"
         for emoji in emojis:
             message += "<:" + emoji.name + ":" + str(emoji.id) + ">"
-        await ctx.send(message)
+        await server.send(message)
