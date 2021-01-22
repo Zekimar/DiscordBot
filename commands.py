afb9a1c9db370966f079ef83cdcc8b9bc47f62a9
Zekimar <zzffcrz@gmail.com>
2021-01-21 19:59:25 -0500

    fixed emojis command

--- commands.py
+++ commands.py
@@ -62,11 +62,11 @@ class server_info(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    #send list of all emojis in the server
     @command()
     async def emojis(self, context):
         emojis = context.guild.emojis
-        message = "context emojis:\n"
+        message = "server emojis:\n"
         for emoji in emojis:
             message += "<:" + emoji.name + ":" + str(emoji.id) + ">"
         await context.send(message)
-        await context.send("There are " + context.guild.emoji_limit - len(emojis) + " emoji slots left")
