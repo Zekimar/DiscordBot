b6b92dfde6b177f649a4db41d18f4bac5eb6a015
Zekimar <zzffcrz@gmail.com>
2021-01-29 21:41:36 -0500

    added descriptions to commands

--- commands.py
+++ commands.py
@@ -14,24 +14,24 @@ class basic_commands(Cog):
     #context = context object given back by API with various data
     #arg = everything said after the command
     # * means give the rest of the text as a single argument
-    @command()
+    @command(brief="repeats message")
     async def mirror(self, context, *, arg):
         await context.send(arg)
 
-    @command()
+    @command(brief="sends PONG!")
     async def ping(self, context):
         await context.message.reply("PONG!")
 
-    @command()
+    @command(brief="sends success!")
     async def test(self, context):
         await context.send("sucess!")
 
-    @command()
+    @command(brief="sends info message")
     async def info(self, context):
         await context.send(info);
 
     #defaults to 6
-    @command()
+    @command(brief="input a number after command to change max")
     async def roll(self, context, max = 6):
         ans = randint(1, max)
         await context.send(ans)
@@ -40,7 +40,7 @@ class basic_commands(Cog):
         if isinstance(error, BadArgument):
             await context.send("ERROR: bad argument")
 
-    @command()
+    @command(brief="flip a coin")
     async def flip(self, context):
         result = randint(0,1)
         if result == 0:
@@ -49,7 +49,7 @@ class basic_commands(Cog):
             await context.send("tails!")
 
     #change nickname and error check
-    @command()
+    @command(brief="change the bot's nickname")
     async def nick(self, context, newnick):
         await context.guild.me.edit(nick = newnick)
         await context.send("nickname changed to " + member.display_name)
@@ -59,7 +59,7 @@ class basic_commands(Cog):
             await context.send("ERROR: missing argument.")
 
     #send list of all emojis in the server
-    @command()
+    @command(brief="list all emojis in the server")
     async def emojis(self, context):
         emojis = context.guild.emojis
         message = "server emojis:\n"
@@ -68,7 +68,7 @@ class basic_commands(Cog):
         await context.send(message)
 
     #displays how long the user @'d in the message has been in the server for
-    @command()
+    @command(brief="@ a user to see when they joined the server")
     async def joined_when(self, context):
         user = context.message.mentions[0]
         await context.send(user.display_name + " has been in the server since " + user.joined_at.__str__())
